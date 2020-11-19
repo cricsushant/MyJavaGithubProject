729efae5fad17d3d9f2a439608e1cf276847aa87
Sushant <sushant1971123@gmail.com>
2020-11-19 17:51:56 +0530

    Second Commit

--- Sample_Java_Project/src/com/lti/demos/ClientApp.java
+++ Sample_Java_Project/src/com/lti/demos/ClientApp.java
@@ -4,5 +4,8 @@ public class ClientApp {
 	public static void main(String[] args) {
 		Person p = new Person("Sushant", 25, 100.12);
 		System.out.println(p);
+		
+		Person p1 = new Person("Jay", 34, 102.1);
+		System.out.println(p1);
 	}
 }
